8b50ca4a4f57a82143181a039385c65f0c17af99
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-12-11 18:00:09 +0900

    Add onCreateNode method

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,7 @@
-exports.onCreateNode = ({ node }) => {
+exports.onCreateNode = ({ node, getNode }) => {
   if (node.internal.type === "MarkdownRemark") {
     console.log("internal type : ", node.internal.type)
+    const fileNode = getNode(node.parent)
+    console.log(`\n`, fileNode.relativePath)
   }
 }
