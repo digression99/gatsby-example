18b2d61f52044eb6896413ac8ffe181e5202cc4a
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-12-12 16:03:39 +0900

    Complete blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,47 @@
-exports.onCreateNode = ({ node, getNode }) => {
+const { createFilePath } = require("gatsby-source-filesystem")
+const path = require("path")
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+
   if (node.internal.type === "MarkdownRemark") {
-    console.log("internal type : ", node.internal.type)
-    const fileNode = getNode(node.parent)
-    console.log(`\n`, fileNode.relativePath)
+    const slug = createFilePath({ node, getNode, basePath: "pages" })
+
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
   }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  console.log("result : ")
+  console.log(JSON.stringify(result, null, 4))
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve("./src/templates/blog-post.jsx"),
+      context: {
+        // data passed to context is available in page queries as graphql variables.
+        slug: node.fields.slug,
+      },
+    })
+  })
 }
